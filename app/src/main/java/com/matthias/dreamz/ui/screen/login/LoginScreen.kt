9545d8188b9e2f708ad36b12aeaad6c001c91631
Matthias <matthiaspinard@gmail.com>
2022-01-07 00:41:02 +0100

    Kick polish log screen

--- app/src/main/java/com/matthias/dreamz/ui/screen/login/LoginScreen.kt
+++ app/src/main/java/com/matthias/dreamz/ui/screen/login/LoginScreen.kt
@@ -1,24 +1,27 @@
 package com.matthias.dreamz.ui.screen.login
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.google.accompanist.insets.ui.Scaffold
 import com.google.accompanist.insets.ui.TopAppBar
 
 @Composable
 fun LoginScreen(loginViewModel: LoginViewModel, navController: NavController) {
     Scaffold(topBar = { TopAppBar(title = { Text("Login") }) }) {
-        Column {
-            Spacer(modifier = Modifier.size(50.dp))
+        Column(horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.spacedBy(40.dp),modifier = Modifier.fillMaxWidth()) {
+            Spacer(modifier = Modifier.size(30.dp))
             TextField(
                 value = loginViewModel.username,
                 onValueChange = { loginViewModel.username = it },
@@ -32,7 +35,7 @@ fun LoginScreen(loginViewModel: LoginViewModel, navController: NavController) {
                 onClick = { loginViewModel.login() },
                 enabled = loginViewModel.username.isNotBlank() && loginViewModel.password.isNotBlank()
             ) {
-                Text("Login")
+                Text("Login", style = MaterialTheme.typography.h6)
             }
             if (loginViewModel.loginError) {
                 Text("Login Error")
